b206288e73610e851836910044e6563bcc120ea6
Stefano Silvestrini <silver.stefano@gmail.com>
2020-04-06 20:31:34 +0200

    db connected, middleware added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Packages
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
+const database = require('./db');
+
+// Middleware
+
+app.use(cors({ credentials: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(process.env.PORT, () => {
-  console.log(`Ready on port ${process.env.PORT}`);
+	console.log(`Ready on port ${process.env.PORT}`);
 });
 
 module.exports = app;
