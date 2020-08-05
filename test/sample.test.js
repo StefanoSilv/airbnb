5c17c4d1c2eb918e6f00f98f3b66f532a1d71e59
Stefano Silvestrini <51782963+StefanoSilv@users.noreply.github.com>
2020-08-05 18:01:41 +0200

    double test checking

--- test/sample.test.js
+++ test/sample.test.js
@@ -4,4 +4,7 @@ describe('Basic Mocha String Test', function () {
   it('testing test', function () {
     assert.equal('Hello'.length, 5);
   });
+  it('testing test number 2', function () {
+    assert.equal('Hello'.length, 4);
+  });
 });
